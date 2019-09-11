4c3ecda100e918f5523738a3d0cbff151ed22ac0
Ubuntu <ubuntu@ip-172-31-21-146.ec2.internal>
2019-09-11 03:04:17 +0000

    Adding changes to content

--- scripts/Content.js
+++ scripts/Content.js
@@ -3,6 +3,6 @@ import * as React from 'react';
 
 export class Content extends React.Component {
     render() {
-        return <div><h1>Hello World from React!</h1></div>;
+        return <div><h1>Hello World from Morgan State!</h1></div>;
     }
 }
